1736628f26417b455556e71cfbaeab81450f4fab
jgzhang3397 <jgzhang1998@gmail.com>
2021-12-07 14:37:17 +0100

    Commit

--- test_P8.c
+++ test_P8.c
@@ -1,37 +1,88 @@
 #include <stdio.h>
-
 int main()
 {
-    // int ch = 0;
-    // while ((ch = getchar())!=EOF)//ctrl+z
+    //Variante1
+    // for(;;)
     // {
-    //     putchar(ch);
+    //     printf("Hehe\n");
     // }
-    int ret = 0;
-    int ch = 0;
-    char password[20]={0};
-    printf("Bitte geben Sie den Password ein:>>>>");
-    scanf("%s", password);// \n = 10 so ret = 10.
-    //缓冲区还剩一个‘\n'
-    //通过getchar（）读取‘\n'
-    //scanf结束标志是空格和回车 wenn password = 123 abc, get error
-    //getchar();
-    while ((ch = getchar()) != '\n')
+    //Variante2
+    int i, j;
+    for (i = 0, j = 0; i < 2 && j<5; i++, ++j)
     {
-        /* code */
-        ;
+        printf("Hehe\n");
     }
-    printf("Bitte bestätigen Sie (Y/N):>>>");
-    ret = getchar();
-    if(ret == 'Y')
-    {
-
-        printf("Alles Klar Vielen Dank!\n");
-    }
-    else
-    {
-        printf("Schade...") ;
-    }
+    
+    // for(int i = 0; i<10; i++)
+    // {
+    //     for(int j = 0; j<10; j++)
+    //     {
+    //         printf("hehe \n");
+    //     }
+    // }
+    // int i = 0;
+    // // 初始化； 判断 ； 调整
+    // for(i=1; i<=10 ; i++)
+    // {
+    //     if (i==5)
+    //     {
+    //         //break;
+    //         continue;
+    //     }
+    //     printf("%d ", i);
+    // }
+    // int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    // for(int i =0; i<10; i++)
+    // {
+    //     printf("%d ",arr[i]);
+    // }
     return 0;
-
 }
+
+// int main()
+// {
+//     int ch = 0;
+//     while ((ch=getchar()) != EOF)
+//     {
+//         if(ch<'0' || ch>'9')//nur 0 ~ 9 ausgeben
+//            continue;
+//         putchar(ch);
+//     }
+    
+// }
+
+// int main()
+// {
+//     // int ch = 0;
+//     // while ((ch = getchar())!=EOF)//ctrl+z
+//     // {
+//     //     putchar(ch);
+//     // }
+//     int ret = 0;
+//     int ch = 0;
+//     char password[20]={0};
+//     printf("Bitte geben Sie den Password ein:>>>>");
+//     scanf("%s", password);// \n = 10 so ret = 10.
+//     //缓冲区还剩一个‘\n'
+//     //通过getchar（）读取‘\n'
+//     //scanf结束标志是空格和回车 wenn password = 123 abc, get error
+//     //getchar();
+//     while ((ch = getchar()) != '\n')
+//     {
+//         /* code */
+//         ;
+//     }
+//     printf("Bitte bestätigen Sie (Y/N):>>>");
+//     ret = getchar();
+//     if(ret == 'Y')
+//     {
+
+//         printf("Alles Klar Vielen Dank!\n");
+//     }
+//     else
+//     {
+//         printf("Schade...") ;
+//     }
+//     return 0;
+
+// }
