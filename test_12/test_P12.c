cb3ff42dae59829e86c0c4ddde8eb3e613d4b067
jgzhang3397 <jgzhang1998@gmail.com>
2021-12-12 20:20:58 +0100

    Funktion mit Rekursion

--- test_12/test_P12.c
+++ test_12/test_P12.c
@@ -1,19 +1,90 @@
 #include<stdio.h>
-//Funktionsrekursion
-//z.B.接受一个整数值（无符号），按照顺序打印他的每一位，例如：输入：1234， 输出：1 2 3 4
-void print(int num)
+// //Funktionsrekursion
+// //z.B.接受一个整数值（无符号），按照顺序打印他的每一位，例如：输入：1234， 输出：1 2 3 4
+// void print(int num)
+// {
+//     if(num>9)
+//     {
+//         print(num/10);
+//     }
+//     printf("%d ", num%10);
+// }
+// int main()
+// {
+//     unsigned int num = 0;
+//     printf("Bitte geben Sie eine Value ein:>");
+//     scanf("%d", &num);
+//     print(num);
+//     return 0;
+// }
+
+// // #include<string.h>
+// // int my_strlen(char* arr)
+// // {
+// //     int count = 0;
+// //     while (*arr != '\0')
+// //     {
+// //         count++;
+// //         arr++;
+// //     }
+// //     return count;
+// // }
+// //Verwendung mit Rekursion
+// //把大事化小
+// //my_strlen("bit")
+// //1+my_strlen("it")
+// //1+1+my_strlen("t");
+// //1+1+1+0
+// int my_strlen(char* arr)
+// {
+//     if(*arr != '\0')
+//     {
+//         return 1+my_strlen(arr+1);
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
+// int main()
+// {
+//     char arr[] ="Hola";
+//     // int len = strlen(arr);
+//     // printf("Sum des Arrays: %d ", len);
+//     int len = my_strlen(arr);
+//     printf("Sum des Arrays: %d ", len);
+//     return 0;
+// }
+
+//求n的阶乘
+//factorial -n
+int fac1(int n)
 {
-    if(num>9)
+    int i = 0;
+    int result = 1;
+    for(i=1; i<=n; i++)
     {
-        print(num/10);
+        result *= i;
     }
-    printf("%d ", num%10);
+    return result;
+}
+//mit Rekursion
+int fac2(int n)
+{
+    if(n<=1)
+       return 1;
+    else
+       return n*fac2(n-1);
 }
 int main()
 {
-    unsigned int num = 0;
+    int n = 0;
+    int result = 0;
     printf("Bitte geben Sie eine Value ein:>");
-    scanf("%d", &num);
-    print(num);
+    scanf("%d", &n);
+    result = fac1(n);
+    printf("mit loop :%d\n", result);
+    result = fac2(n);
+    printf("mit Rekursion :%d\n", result);
     return 0;
 }
