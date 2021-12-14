f99404a555ca7f94fd51d0fdd0ca500c8f431089
jgzhang3397 <jgzhang1998@gmail.com>
2021-12-14 14:45:13 +0100

    Funktion mit Rekursion

--- test_12/test_P12.c
+++ test_12/test_P12.c
@@ -56,35 +56,114 @@
 //     return 0;
 // }
 
-//求n的阶乘
-//factorial -n
-int fac1(int n)
+// //求n的阶乘
+// //factorial -n
+// int fac1(int n)
+// {
+//     int i = 0;
+//     int result = 1;
+//     for(i=1; i<=n; i++)
+//     {
+//         result *= i;
+//     }
+//     return result;
+// }
+// //mit Rekursion
+// int fac2(int n)
+// {
+//     if(n<=1)
+//        return 1;
+//     else
+//        return n*fac2(n-1);
+// }
+// int main()
+// {
+//     int n = 0;
+//     int result = 0;
+//     printf("Bitte geben Sie eine Value ein:>");
+//     scanf("%d", &n);
+//     result = fac1(n);
+//     printf("mit loop :%d\n", result);
+//     result = fac2(n);
+//     printf("mit Rekursion :%d\n", result);
+//     return 0;
+// }
+
+// //斐波那契数(Fibonacci sequence)
+// //Fib(0)=0
+// //Fib(1)=1
+// //Fib(n)=Fib(n-1)+Fib(n-2)(n>=2)
+// //Es dauer so lange
+// // int Fib(int n)
+// // {
+// //     if(n>=2)
+// //     {
+// //         return (Fib(n-1)+Fib(n-2));
+// //     }
+// //     else 
+// //     {
+// //         return n;
+// //     }
+// // }
+// //Optimierung
+// int Fib(int n)
+// {
+//     int a = 1;
+//     int b = 1;
+//     int c = 1;
+//     while(n>2)
+//     {
+//         c = a+b;
+//         a = b;
+//         b = c;
+//         n--;
+//     }
+//     return c;
+// }
+// int main()
+// {
+//     int result = 0;
+//     int n = 0;
+//     printf("Bitte geben Sie eine Value ein n:>");
+//     scanf("%d", &n);
+//     result = Fib(n);
+//     printf("%d ", result);
+//     return 0;
+// }
+
+// Ähnliche Frage 青蛙跳台阶 jump_floor
+// int jump_floor(int n)
+// {
+//     if(n>3)
+//     {
+//         return jump_floor(n-1)+jump_floor(n-2);
+//     }
+//     else
+//     {
+//         return n;
+//     }
+// }
+int jump_floor(int n)
 {
-    int i = 0;
-    int result = 1;
-    for(i=1; i<=n; i++)
+    int a = 1;
+    int b = 1;
+    int c = 1;
+    while (n>=2)
     {
-        result *= i;
+        c = a+b;
+        a = b;
+        b = c;
+        n--;
     }
-    return result;
-}
-//mit Rekursion
-int fac2(int n)
-{
-    if(n<=1)
-       return 1;
-    else
-       return n*fac2(n-1);
+    return c;
+    
 }
 int main()
 {
     int n = 0;
-    int result = 0;
-    printf("Bitte geben Sie eine Value ein:>");
+    int ret = 0;
+    printf("Bitte geben Sie die Value von Floor ein:>");
     scanf("%d", &n);
-    result = fac1(n);
-    printf("mit loop :%d\n", result);
-    result = fac2(n);
-    printf("mit Rekursion :%d\n", result);
-    return 0;
+    ret = jump_floor(n);
+    printf("Insgamt %d : mal Jump\n", ret);
 }
