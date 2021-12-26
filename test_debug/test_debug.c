8ff517d26752e25e26ea7909b776cfb486d534c2
jgzhang3397 <jgzhang1998@gmail.com>
2021-12-26 18:17:46 +0100

    ueber wie kann debuggen und optimeren

--- test_debug/test_debug.c
+++ test_debug/test_debug.c
@@ -62,24 +62,67 @@
 //     *dest = *src;
 // }
 //=============Optimerung weiter
+// #include<assert.h>
+// char* my_strcpy(char* dest, const char* src)
+// {
+//     char* ret = dest;
+//     assert(dest != NULL);//断言
+//     assert(src != NULL);
+//     // if(dest != NULL && src != NULL)
+//     // {
+//         //把src指向的字符串拷贝到dest指向的空间，包含‘\0’字符
+//         while (*dest++ = *src++)
+//         {
+//             ;
+//         }
+//     // }
+//     return ret;
+// }
+// int main()
+// {
+//     //strcpy
+//     char arr1[]="#######################";
+//     char arr2[]="bit";
+//     my_strcpy(arr1, arr2);
+//     printf("%s\n", arr1);
+// }
+
+// int main()
+// {
+//     const int num = 10;//num kann nicht aendern
+//     //const int* p = &num;
+//     // *p = 20;//error const 放在指针变量的*左边时，修饰的是 *p，不能通过p来改变*p（num）的值
+
+//     // int* const p = &num;
+//     // //const 放在*右边，修饰的是指针变量p本身，p不能被改变
+//     // int n = 100;
+//     // p = &n;
+
+//     const int* const p = &num;
+//     *p = 20;
+//     int n = 100;
+//     p = &n;
+//     printf("%d\n", num);
+//     //printf("%d\n", *p);
+//     return 0;
+// }
+
 #include<assert.h>
-void my_strcpy(char* dest, char* src)
+int my_strlen(const char* arr)//*arr 的内容不被改变
 {
-    assert(dest != NULL);
-    assert(src != NULL);
-    // if(dest != NULL && src != NULL)
-    // {
-        while (*dest++ = *src++)
-        {
-            ;
-        }
-    // }
+    int count = 0;
+    assert(arr != NULL);//保证指针的有效性 不能为空指针
+    while (*str != '\0')
+    {
+        count++;
+        *str++;
+    }
+    return count;
 }
 int main()
 {
-    //strcpy
-    char arr1[]="#######################";
-    char arr2[]="bit";
-    my_strcpy(arr1, arr2);
-    printf("%s\n", arr1);
+    char arr[]="abcdef";
+    int len = my_strlen(arr);
+    printf("%d\n", len);
+    return 0;
 }
