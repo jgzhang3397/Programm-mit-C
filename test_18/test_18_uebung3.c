191636d3910feb6a348ee9482fe7a215626516da
jgzhang3397 <jgzhang1998@gmail.com>
2022-01-04 23:49:36 +0100

    指针面试题

--- test_18/test_18_uebung3.c
+++ test_18/test_18_uebung3.c
@@ -5,4 +5,29 @@ int main()
 {
     //二维数组
     int a[3][4] = {0};
+
+    printf("%lu\n", sizeof(a));//3*4*4 = 48
+    printf("%lu\n", sizeof(a[0][0]));//1*4 = 4
+    printf("%lu\n", sizeof(a[0]));//4*4 = 16 a[0]第一行作为一维数组的数组名，sizeof(a[0])单独放在sizeof（）内
+    //a[1]第二行的数组名
+    printf("%lu\n", sizeof(a[0] + 1));//4\8 地址 a[0]没有单独放在sizeof中，表示首元素地址 1行第一个
+    //a[0] + 1 ：1行第二个元素地址
+    printf("====\n");
+    printf("%p\n", &a[0][0]);
+    printf("%p\n", a[0] + 1);
+    printf("%p\n", a + 1);
+    printf("%p\n", &a[0] + 1);
+    printf("====\n");
+
+    printf("%lu\n", sizeof(*a[0] + 1));//4  1行第二个元素地址解引用
+    printf("%lu\n", sizeof(a + 1));//4\8 a 首元素地址 ，二维数组首地址就是第一行 a就是第一行地址 a + 1 表示第二行地址
+
+    printf("%lu\n", sizeof(*(a + 1)));//4*4 = 16 第二行地址解引用
+    printf("%lu\n", sizeof(&a[0] + 1));//4\8 地址 第二行的地址
+    printf("%lu\n", sizeof(*(&a[0] + 1)));//4*4 = 16 第二行地址解引用
+    printf("%lu\n", sizeof(*a));//4*4 = 16 第一行地址解引用
+    printf("%lu\n", sizeof(a[3]));//4*4 = 16 sizeof内部表达式不访问，计算第四行数组大小
+
+    return 0;
+
 }
