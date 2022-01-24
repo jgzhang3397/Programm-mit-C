7602cd88bcef5deff1f56a260fde4820564a4362
jgzhang3397 <jgzhang1998@gmail.com>
2022-01-24 23:58:03 +0100

    mem-function

--- test_19/demo05.c
+++ test_19/demo05.c
@@ -88,6 +88,7 @@
         void* - 通用类型的指针-无类型指针
 
 */
+
 #include<string.h>
 #include<assert.h>
 void test1()
@@ -133,7 +134,7 @@ void* my_memcpy(void* dest, const void* src, size_t num)
     {
         *(char*)dest = *(char*)src;
         (char*)dest++;
-        (char*)src++;
+        (char*)src++;//先强转，后++
     }
     return ret;
 }
@@ -147,9 +148,98 @@ void test2()
     printf("%s, %d\n", arr2[0].name, arr2[0].age);
 }
 
+/*
+dest & src地址大小比较：确定拷贝顺序
+    dest<src src的m个数据从前向后拷贝
+    dest>src&&dest<src+count src的m个数据从后向前拷贝
+    dest>src+count, 两种拷贝方法都可
+*/
+void* my_memmove(void* dest, const void* src, size_t num)
+{
+    assert(dest && src);
+    void* ret = dest;
+    if(dest<src)
+    {
+        //前->后
+        while (num--)
+        {
+            *(char*)dest = *(char*)src;
+            (char*)dest++;
+            (char*)src++;
+        }
+        
+    }
+    else
+    {
+        //后->前
+        //先找的最后一个字节的地址 dest + num -1
+        while (num--)//20 - 1= 19
+        {
+            *((char*)dest + num) = *((char*)src + num);
+        }
+        
+    }
+
+    // if(dest<src || dest>(char*)src+count)
+    // {
+    //     //前->后
+    // }
+    // else
+    // {
+    //     //后->前
+    // }
+    return ret;
+}
+
+void test3()
+{
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int i = 0;
+    int len = sizeof(arr)/sizeof(arr[0]);
+    //my_memcpy(arr+2, arr, 20);//在arr中从arr[2]向后拷贝5个值到arr里，20个字节
+    //1 2 1 2 1 2 1 8 9 10
+
+    //理想值：1 2 1 2 3 4 5 8 9 10
+    //原因：找不到3，4，5的值，被1，2取代
+    //解决办法：memmove()
+    //memmove(arr+2, arr, 20);//1 2 1 2 3 4 5 8 9 10
+    //memcpy(arr+2, arr, 20);//1 2 1 2 1 2 1 8 9 10
+    my_memmove(arr+2, arr, 20);
+    for ( i = 0; i <len ; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    
+}
+
+void test4()
+{
+    //01 00 00 00 02 00 00 00 03 00 00 00
+    int arr1[] = {1, 2, 3, 4, 5};
+    int arr2[] = {1, 2, 5, 4, 3};
+    //01 00 00 00 02 00 00 00 05 00 00 00
+    int ret = memcmp(arr1, arr2, 9);
+    printf("%d\n", ret);//-2
+}
+
+void test5()
+{
+    int arr[10] = {0};
+    memset(arr, '*', 10);//10 是字节数
+    int len = sizeof(arr)/sizeof(arr[0]);
+    for (int i = 0; i < len; i++)
+    {
+        printf("%c ", arr[i]);
+    }
+    
+}
+
 int main()
 {
-    //test1();
-    test2();
+    //test1();//memcpy
+    //test2();//my_memcoy
+    //test3();//memmove && my_memmove
+    //test4();//memcmp
+    test5();//memset
     return 0;
 }
