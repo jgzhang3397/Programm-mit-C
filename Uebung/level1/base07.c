41432821cf7f08355ecf2b20eddaaebd471197be
jgzhang3397 <jgzhang1998@gmail.com>
2022-03-18 08:15:12 +0100

    base07 finish

--- Uebung/level1/base07.c
+++ Uebung/level1/base07.c
@@ -328,6 +328,261 @@ void Test05()
         p= NULL;
     }
 }
+
+/*
+Test06():
+    描述
+    有一个有序数字序列，从小到大排序，将一个新输入的数插入到序列中，保证插入新数后，序列仍然是升序。
+
+    输入描述：
+    第一行输入一个整数N(0≤N≤50)。
+
+    第二行输入N个升序排列的整数，输入用空格分隔的N个整数。
+
+    第三行输入想要进行插入的一个整数。
+
+    输出描述：
+    输出为一行，N+1个有序排列的整数。
+
+    示例1
+        输入：  
+        5
+        1 6 9 22 30
+        8
+        输出：
+        1 6 8 9 22 30
+*/
+void insert_int(int* p, int n, int m)
+{
+    int i, j;
+    for(i=0; i<n; i++)
+    {
+        if(m < *(p+i) )//定位插入位置
+        {
+            for(j=n; j>i; j--)//后面全都往后窜一位
+            {
+                *(p+j) = *(p+j-1);
+            }
+            *(p+i) = m;//第i位放插入数m
+            break;
+        }
+        *(p+n) = m; //要插入的值比所有原数组中所元素都大，放在末尾
+    }
+}
+
+void my_print(int* p, int n)
+{
+    int i;
+    for(i=0; i<n; i++)
+    {
+        printf("%d ", *(p+i));
+    }
+    printf("\n");
+}
+
+void Test06()
+{
+    int n, i, m;
+    int* p = NULL;
+    while(scanf("%d", &n)!=EOF && (0<=n && n<= 50))
+    {
+        p = (int *)malloc((n+1)*sizeof(int));
+        for(i=0; i<n; i++)
+        {
+            scanf("%d", p+i);
+        }
+        scanf("%d", &m);
+        insert_int(p, n, m);
+        my_print(p, n+1);
+
+        free(p);
+        p = NULL;
+    }
+}
+
+/*
+Test07():
+    描述
+    有一个整数序列（可能有重复的整数），现删除指定的某一个整数，输出删除指定数字之后的序列，序列中未被删除数字的前后位置没有发生改变。
+
+    数据范围：序列长度和序列中的值都满足 1≤n≤50
+
+    输入描述：
+    第一行输入一个整数(0≤N≤50)。
+
+    第二行输入N个整数，输入用空格分隔的N个整数。
+
+    第三行输入想要进行删除的一个整数。
+
+    输出描述：
+    输出为一行，删除指定数字之后的序列。
+
+    示例1
+        输入：
+            7
+            1 2 3 4 4 5 9
+            4
+        输出：
+            1 2 3 5 9
+*/
+
+void print(int * ps, int count)
+{
+    int i;
+    for(i=0; i<count; i++)//创建count长度的数组，没有和m相等的元素
+    {
+        printf("%d ", *(ps+i));
+    }
+    printf("\n");
+}
+void delete_num(int * ps, int n, int m)
+{
+    int i, count=0;
+    for(i=0; i<n; i++)
+    {
+        if(*(ps+i)!=m)//如果不是要删除的那个元素，则数组正常遍历，如果找到了，就覆盖
+        {
+            *(ps+count) = *(ps+i);
+            count++;
+        }
+    }
+    print(ps, count);  
+}
+
+void Test07()
+{
+    int n, i, m;
+    int * ps = NULL;
+    while(scanf("%d", &n)!=EOF && (1<=n && n<=50))
+    {
+        ps = (int *)malloc(n*sizeof(int));
+        for(i=0; i<n; i++)
+        {
+            scanf("%d", ps+i);
+        }
+        scanf("%d", &m);
+        delete_num(ps, n, m);
+
+        free(ps);
+        ps = NULL;
+    }
+}
+
+/*
+Test08():
+    描述
+    输入n个整数的序列，要求对这个序列进行去重操作。
+    所谓去重，是指对这个序列中每个重复出现的整数，
+    只保留该数第一次出现的位置，删除其余位置。
+
+    输入描述：
+    输入包含两行，第一行包含一个正整数n（1 ≤ n ≤ 1000），
+    表示第二行序列中数字的个数；第二行包含n个整数（范围1~5000），用空格分隔。
+
+    输出描述：
+    输出为一行，按照输入的顺序输出去重之后的数字，用空格分隔。
+
+    示例1
+    输入：
+    5
+    10 12 93 12 75
+    输出：
+    10 12 93 75
+*/
+void delete_repeat_num(int* ps, int n)
+{
+    int i, j, flag=1;
+    
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<i; j++)
+        {
+            if(*(ps+j)==*(ps+i))
+                flag = 0;
+        }
+        if(flag)
+            printf("%d ", *(ps+i));
+        flag = 1;
+    }
+    printf("\n");
+}
+
+void Test08()
+{
+    int i,n;
+    int* ps = NULL;
+    while(scanf("%d", &n)!=EOF && (1<=n && n<=5000))
+    {
+        ps = (int *)malloc(n*sizeof(int));
+        for(i=0; i<n; i++)
+        {
+            scanf("%d", ps+i);
+        }
+        delete_repeat_num(ps, n);
+        
+        free(ps);
+        ps = NULL;
+    }
+}
+
+/*
+Test09():
+    描述
+    用筛选法求n以内的素数。筛选法求解过程为：将2~n之间的正整数放在数组内存储，
+    将数组中2之后的所有能被2整除的数清0，再将3之后的所有能被3整除的数清0 ，
+    以此类推，直到n为止。数组中不为0 的数即为素数。
+
+    输入描述：
+    多组输入，每行输入一个正整数（不大于100）。
+
+    输出描述：
+    针对每行输入的整数n，输出两行，第一行，输出n之内（包括n）的素数，用空格分隔，
+
+    第二行，输出数组中2之后被清0 的个数。每行输出后换行。
+
+    i = 2; ps+i = 4
+*/
+
+void get_prime_num(int * ps, int n)
+{
+    int i, j, count=0;
+    for(i=0; i<n-1; i++)
+    {
+        *(ps+i) = i+2;//将2～n放在数组内
+
+        for(j = 2; j<*(ps+i); j++)
+        {
+            if(*(ps+i)%j == 0)
+            {
+                *(ps+i) = 0;
+                break;//此处的作用在于：减少递归，即数组元素被2～n任意整除都不为素数，所以可以break直接终止循环
+            }
+        }
+    }
+    
+    for(i = 0; i<n-1; i++)
+    {
+        if(*(ps+i)!=0)
+            printf("%d ", *(ps+i));
+        else
+            count++;
+    }
+    printf("\n%d\n", count);
+}
+
+void Test09()
+{
+    int n;
+    int * ps = NULL;
+    while(scanf("%d", &n)!=EOF && (1<=n && n<=100))
+    {
+        ps = (int *)malloc(n*sizeof(int));
+        get_prime_num(ps, n);
+
+        free(ps);
+        ps = NULL;
+    }
+}
 int main(void)
 {
     //Test01();
@@ -335,6 +590,10 @@ int main(void)
     //Test03_S1();
     //Test03_S2();
     //Test04();
-    Test05();
+    //Test05();
+    //Test06();
+    //Test07();
+    //Test08();
+    Test09();
     
 }
