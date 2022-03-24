e0245f633d5b30ad89ee9a1f1c6513d644243b7c
jgzhang3397 <jgzhang1998@gmail.com>
2022-03-24 19:12:34 +0100

    my_strlen() & my_strcpy() & my_strcat()

--- test_19/demo01.c
+++ test_19/demo01.c
@@ -3,6 +3,7 @@
 #include<assert.h>
 
 /*
+Test01():
 size_t strlen(const char* str);
     注意事项:
     1. 字符串以'\0' 作为结束标志， strlen函数返回的是在字符串中'\0'前面出现的字符个数（不包含'\0'）
@@ -22,42 +23,41 @@ int my_strlen(const char* str)
     return count;
 }
 
-// int main()
-// {
-//     int len = my_strlen("abcdef");
-//     //错误示范
-//     // char arr[] = {'a', 'b', 'c', 'd', 'e'};
-//     // int len = strlen(arr);
-//     printf("%d\n", len);
-//     return 0;
-// }
+void Test01()
+{
+    int len = my_strlen("abcdef");
+    //错误示范
+    //char arr[] = {'a', 'b', 'c', 'd', 'e'};
+    //int len = strlen(arr);
+    printf("%d\n", len);
+}
 
-// int main()
-// {
-//     //strlen的返回值是size_t == unsigned int >0
-//     // 3 - 6 == -3(unsigned int) ==> > 0
-//     if(strlen("abc") - strlen("abcdef") >0)
-//     {
-//         printf("hehe\n");
-//     }
-//     else
-//     {
-//         printf("haha\n");
-//     }
-//     printf("======================\n");
-//     if(my_strlen("abc") - my_strlen("abcdef") >0)
-//     {
-//         printf("hehe\n");
-//     }
-//     else
-//     {
-//         printf("haha\n");
-//     }
-//     return 0;
-// }
+void Test02()
+{
+    //strlen的返回值是size_t == unsigned int >0
+    // 3 - 6 == -3(unsigned int) ==> > 0
+    if(strlen("abc") - strlen("abcdef") >0)
+    {
+        printf("hehe\n");
+    }
+    else
+    {
+        printf("haha\n");
+    }
+    printf("======================\n");
+    if(my_strlen("abc") - my_strlen("abcdef") >0)
+    {
+        printf("hehe\n");
+    }
+    else
+    {
+        printf("haha\n");
+    }
+}
 
 /*
-char* strcpy(char* destination, const char* source);
+Test03():
+    char* strcpy(char* destination, const char* source);
     注意事项：
     1. 源字符串必须以'\0'结束
     2. 会将源字符串中的'\0'拷贝到目标空间
@@ -81,31 +81,32 @@ char* my_strcpy(char* dest, const char* src)//const 修饰 指针src不发生改
     //*dest = *src;//'\0' cpy
     
     //拷贝src指向的字符串到dest指向的空间，包含‘\0'
-    while (*dest++ = *src++)
+    while (*dest++ == *src++)
     {
         ;
     }
     //返回目的空间的起始地址
     return ret; 
 }
-// int main()
-// {
-//     char arr1[] = "abcdefghi";
-//     char arr2[] = "hello";
 
-//     //错误示范
-//     //char *arr1 = "abcdefghi"; //指针arr1指向的是常量字符串，不可改变
-//     // char arr2[] = {'a', 'b', 'v'};//此 arr2[]没有'\0'
-//     // strcpy(arr1, arr2);
-//     // printf("%s\n", arr1);
+void Test03()
+{
+    char arr1[] = "abcdefghi";
+    char arr2[] = "hello";
+
+    //错误示范
+    // char *arr1 = "abcdefghi"; //指针arr1指向的是常量字符串，不可改变
+    // char arr2[] = {'a', 'b', 'v'};//此 arr2[]没有'\0'
+    // strcpy(arr1, arr2);
+    // printf("%s\n", arr1);
     
-//     my_strcpy(arr1, arr2);
-//     printf("%s\n", arr1);
-//     return 0;
-// }
+    my_strcpy(arr1, arr2);
+    printf("%s\n", arr1);
+}
 
 /*
-char* strcat (char* destination, const char* source);
+Test04():
+    char* strcat (char* destination, const char* source);
     注意事项：
     1. 源字符串必须以'\0'结束
     2. 目标空间必须足够大，能容纳下源字符串的内容
@@ -125,19 +126,34 @@ char* my_strcat(char* dest, const char* src)
         dest ++;
     }
     //2. 追加
-    while (*dest++ = *src++)
+    // while (*dest++ = *src++)
+    // {
+    //     ;
+    // }
+    while (*src != '\0')
     {
-        ;
+        *dest++ = *src++;
     }
     return ret;
 }
-int main()
+
+void Test04()
 {
     char arr1[20] = "hello ";
     char arr2[] = "world";
-    strcat(arr1, arr2);
+
+    // strcat(arr1, arr2);
     // printf("%s\n", arr1);
+
     my_strcat(arr1, arr2);
     printf("%s\n", arr1);
+}
+
+int main()
+{
+    //Test01();
+    //Test02();
+    //Test03();
+    Test04();
     return 0;
 }
