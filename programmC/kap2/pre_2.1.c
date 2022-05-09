9cbbcf0f15f4f8660a5c31f305b6a0c301b29e93
jgzhang3397 <jgzhang1998@gmail.com>
2022-05-09 19:08:15 +0200

    add

---
+++ programmC/kap2/pre_2.1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<limits.h>
+/*
+确定signed / unsigned char, short, int, long 取值范围
+*/
+int main()
+{
+    printf("Size of signed Char %d\n", CHAR_BIT);
+    printf("Size of signed Char Max %d\n", CHAR_MAX);
+    printf("Size of signed Char Min %d\n", CHAR_MIN);
+    printf("Size of unsigned Char Max %u\n", UCHAR_MAX);
+    printf("\n\n");
+
+    //printf("Size of signed Short %d\n", SHRT_BIT);
+    printf("Size of signed Short Max %d\n", SHRT_MAX);
+    printf("Size of signed Short Min %d\n", SHRT_MIN);
+    printf("Size of unsigned Short Max %u\n", USHRT_MAX);
+    printf("\n\n");
+
+    //printf("Size of signed Int %d\n", INT_BIT);
+    printf("Size of signed Int Max %d\n", INT_MAX);
+    printf("Size of signed Int Min %d\n", INT_MIN);
+    printf("Size of unsigned Int Max %u\n", UINT_MAX);
+    printf("\n\n");
+
+    printf("Size of signed Long Max %d\n", LONG_BIT);
+    printf("Size of signed Long Max %ld\n", LONG_MAX);
+    printf("Size of signed Long Min %ld\n", LONG_MIN);
+    printf("Size of unsigned Long Max %lu\n", ULONG_MAX);
+
+    return 0;
+}
